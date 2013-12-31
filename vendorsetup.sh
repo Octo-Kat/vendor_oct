4734f2fdc8f43c7ac74cfdd7c8dc13d633b5c3d5
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-31 11:10:34 -0500

    Mmm time for lunch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,3 +20,10 @@ add_lunch_combo oct_m7spr-userdebug
 add_lunch_combo oct_m7tmo-userdebug
 add_lunch_combo oct_m7ul-userdebug
 add_lunch_combo oct_m7vzw-userdebug
+add_lunch_combo oct_jewel-userdebug
+add_lunch_combo oct_evita-userdebug
+add_lunch_combo oct_grouper-userdebug
+add_lunch_combo oct_tilapia-userdebug
+add_lunch_combo oct_hammerhead-userdebug
+add_lunch_combo oct_mako-userdebug
+add_lunch_combo oct_flo-userdebug
