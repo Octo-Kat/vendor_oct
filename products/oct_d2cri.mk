bba4415c67a96d32661aeb70193ddd73e63877b7
Carl Windhausen <cjwindhausen@gmail.com>
2014-02-18 11:39:32 -0500

    derp derp

--- products/oct_d2cri.mk
+++ products/oct_d2cri.mk
@@ -17,7 +17,7 @@ ifeq (oct_d2cri,$(TARGET_PRODUCT))
 
 # Inherit some common OctOS stuff.
 $(call inherit-product, vendor/oct/config/cdma.mk)
-$(call inherit-product, vendor/oct/config/common_full_phone.mk
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
 # Inherit AOSP device configuration
 $(call inherit-product, device/samsung/d2cri/full_d2cri.mk)
