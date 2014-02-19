0f41e2c2277ebe661d14a4c28bab003a2d12c3cb
Carl Windhausen <cjwindhausen@gmail.com>
2014-02-19 11:36:41 -0500

    add d2lte

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq (oct_d2lte,$(TARGET_PRODUCT))
+    PRODUCT_MAKEFILES += $(LOCAL_DIR)/oct_d2lte.mk
+endif
 ifeq (oct_d2att,$(TARGET_PRODUCT))
     PRODUCT_MAKEFILES += $(LOCAL_DIR)/oct_d2att.mk
 endif
