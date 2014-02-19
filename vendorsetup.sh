9567d04d6119980377563f2ef884ff0ce1af167c
Carl Windhausen <cjwindhausen@gmail.com>
2014-02-19 11:35:21 -0500

    add unified builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -28,3 +28,5 @@ add_lunch_combo oct_tilapia-userdebug
 add_lunch_combo oct_hammerhead-userdebug
 add_lunch_combo oct_mako-userdebug
 add_lunch_combo oct_flo-userdebug
+add_lunch_combo oct_jflte-userdebug
+add_lunch_combo oct_d2lte-userdebug
