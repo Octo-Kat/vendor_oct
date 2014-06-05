4eba8350dea063196f8620be4bae49f607d6c6ea
treken <chupekevin@gmail.com>
2014-06-05 08:28:28 -0400

    Add bootanimation to build

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -1,5 +1,9 @@
 # Inherit common stuff
 $(call inherit-product, vendor/oct/config/common.mk)
 
+# Add Bootanimation
+PRODUCT_COPY_FILES += \
+	vendor/oct/prebuilt/common/bootanimation/bootanimation.zip:system/media/bootanimation.zip
+
 # Add apns configs
 $(call inherit-product, vendor/oct/config/telephony.mk)
