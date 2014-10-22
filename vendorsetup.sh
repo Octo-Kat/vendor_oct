CarbonROM/android_vendor_carbon

vendorsetup.sh: add hltevzw
Change-Id: Ia6e55be7c08c06a9472ba8c90ca26f2adcc3cda3
Signed-off-by: mattmanwrx <mbloomfield87@gmail.com>

vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hercules-userdebug
 add_lunch_combo carbon_hlte-userdebug
 add_lunch_combo carbon_hltespr-userdebug
+add_lunch_combo carbon_hltevzw-userdebug
 add_lunch_combo carbon_i605-userdebug
 add_lunch_combo carbon_i9100-userdebug
 add_lunch_combo carbon_i9300-userdebug

Octo-Kat/vendor_oct

Conflicts:
	vendorsetup.sh

vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo oct_hammerhead-userdebug
 add_lunch_combo oct_hercules-userdebug
 add_lunch_combo oct_hlte-userdebug
 add_lunch_combo oct_hltespr-userdebug
+add_lunch_combo oct_hltevzw-userdebug
 add_lunch_combo oct_i605-userdebug
 add_lunch_combo oct_i9082-userdebug
 add_lunch_combo oct_i9100-userdebug