CarbonROM/android_vendor_carbon

vendorsetup: add hlteusc
Change-Id: I693d8b3b9733406e771c95b38b66fb67f4dc2031
Signed-off-by: mattmanwrx <mbloomfield87@gmail.com>

vendorsetup.sh
@@ -13,6 +13,7 @@ add_lunch_combo carbon_hammerhead-userdebug
 add_lunch_combo carbon_hercules-userdebug
 add_lunch_combo carbon_hlte-userdebug
 add_lunch_combo carbon_hltespr-userdebug
+add_lunch_combo carbon_hlteusc-userdebug
 add_lunch_combo carbon_hltevzw-userdebug
 add_lunch_combo carbon_i605-userdebug
 add_lunch_combo carbon_i9100-userdebug

Octo-Kat/vendor_oct

Conflicts:
	vendorsetup.sh

vendorsetup.sh
@@ -14,6 +14,7 @@ add_lunch_combo oct_hercules-userdebug
 add_lunch_combo oct_hlte-userdebug
 add_lunch_combo oct_hltespr-userdebug
 add_lunch_combo oct_hltevzw-userdebug
+add_lunch_combo oct_hlteusc-userdebug
 add_lunch_combo oct_i605-userdebug
 add_lunch_combo oct_i9082-userdebug
 add_lunch_combo oct_i9100-userdebug